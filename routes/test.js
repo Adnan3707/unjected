b9014defe27f42e4afeeda6b098d289d2f69defc
Farooq <adnanfarooq37@gmail.com>
2023-11-21 01:35:58 +0530

    test

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,9 @@
 const db = require(process.env.data_base);
 const express = require('express');
 const router = express.Router();
-router.get('/all', (req, res) => {
+router.get('/all',async (req, res) => {
     try{
-        let user = db.us_user.findAll();
+        let user = await db.us_user.findAll({});
      return res.status(200).json({'user' :  user})
    }catch(err){
      return res.status(400).json({ 'server error':err })
